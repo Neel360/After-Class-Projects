2e78cdc46ef7a7f2d2eabf941537b46fc9248da2
neel-guckool <neel001g@gmail.com>
2026-05-26 09:17:11 +0100

    Python

---
+++ storing_birthdays.py
@@ -0,0 +1,11 @@
+harry = "01/01/1990"
+emily = "02/02/1991"
+tom = "03/03/1992"
+michael = "04/04/1993"
+peter = "05/05/1994"
+
+print(f"Harry's birthday is on {harry}")
+print(f"Emily's birthday is on {emily}")
+print(f"Tom's birthday is on {tom}")
+print(f"Michael's birthday is on {michael}")
+print(f"Peter's birthday is on {peter}")
